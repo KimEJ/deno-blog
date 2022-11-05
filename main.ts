b168fd6873918a4f1861fc81d308cf8bff106224
김어진 <kimuj5090@gmail.com>
2022-11-05 11:20:42 +0900

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,13 @@
+import blog from "https://deno.land/x/blog@0.5.0/blog.tsx";
+
+blog({
+  title: "짱귀여운 아가블로그",
+  author: "KimEJ",
+  avatar: "./avatar.jpg",
+  avatarClass: "full",
+  links: [
+    { title: "Email", url: "mailto:kimuj5090@gamil.com" },
+    { title: "GitHub", url: "https://github.com/KimEJ" },
+  ],
+  background: "#fff"
+});
